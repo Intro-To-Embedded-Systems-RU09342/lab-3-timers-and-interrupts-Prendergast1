1e62137b406966ac2694b1bbfcfb64c80ecfbb9c
Prendergast1 <47788939+Prendergast1@users.noreply.github.com>
2019-10-15 10:47:21 -0400

    Add files via upload

--- TIMER A Blink/MSP430FR6989/main.c
+++ TIMER A Blink/MSP430FR6989/main.c
@@ -1,9 +1,8 @@
 // Author: Sean Prendergast
-// Code was initially started on September 18th, 2019
+// Code was initially started on September 26th, 2019
 // Code was last modified on October 13th, 2019
 // Submitted October 17th, 2019
 // Exercise: Timer A for MSP430FR6989
-
 
 #include <msp430.h>
 
@@ -15,25 +14,27 @@ int main(void)
                                                 to activate configured port settings*/
 
       P1DIR |= BIT0;                            // Set P1.0 to output direction
-      P9DIR |= BIT7;                            // Set P1.6 to output direction
+      P9DIR |= BIT7;                            // Set P9.7 to output direction
       P1OUT |= ~BIT0;                           // Clear the first LED (P1.0) off
       P9OUT |= ~BIT7;                           // Clear the second LED (P9.7) off
 
-      TA0CCTL0 = CCIE; // Enable interrupt in compare mode
-      TA0CTL = TASSEL_2 + MC_1 + ID_2; // SMCLK/4, Up
-      TA0CCR0 = 250000/ 5; // 250000 / 5 = 50000, (10^6 [Hz] / 4) / (50000) = 5 Hz
+      // Timer 0
+      TA0CCTL0 = CCIE;                          // Enable interrupt in compare mode
+      TA0CTL = TASSEL_2 + MC_1 + ID_2;          // SMCLK/4, Up
+      TA0CCR0 = 250000/ 5;                      // 250000 / 5 = 50000, (10^6 [Hz] / 4) / (50000) = 5 Hz
 
-      TA1CCTL0 = CCIE; // Enable interrupt in compare mode
-      TA1CTL = TASSEL_2 + MC_1 + ID_2; // SMCLK/4, Up
-      TA1CCR0 = 250000/ 10; // 250000 / 10 = 25000, (10^6 [Hz] / 4) / (25000) = 10Hz
+      // Timer 1
+      TA1CCTL0 = CCIE;                          // Enable interrupt in compare mode
+      TA1CTL = TASSEL_2 + MC_1 + ID_2;          // SMCLK/4, Up
+      TA1CCR0 = 250000/ 10;                     // 250000 / 10 = 25000, (10^6 [Hz] / 4) / (25000) = 10Hz
 
 
-      __enable_interrupt();
+      __enable_interrupt();                 // Enable interrupt
 
-      __bis_SR_register(LPM0 + GIE);
+      __bis_SR_register(LPM0 + GIE);        // Input LPMO (Low-power mode 0) with GIE (General Interrupt Enable)
 }
 
-
+// Timer 0 Interrupt
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void TIMER_A0(void)
 
@@ -42,6 +43,7 @@ __interrupt void TIMER_A0(void)
 
     }
 
+// Timer 1 Interrupt
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void TIMER_A1(void)
 
